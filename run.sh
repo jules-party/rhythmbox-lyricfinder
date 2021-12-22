caaca7ab5324e71c467920d6d43403a6030103a0
æ <83989081+crue-ton@users.noreply.github.com>
2021-12-22 09:42:02 -0500

    - Added Colour -

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-echo 'Running Python File...'
-python main.py
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+printf "${BLUE}Running ${YELLOW}Python ${BLUE}File...\n${NC}"
+python /home/geck/Py/rhythmbox-lyricfinder/main.py
